083f330d6df8bb50174c8f0114deff759457c6c4
Njool45 <42964824+Njool45@users.noreply.github.com>
2018-09-10 18:12:25 +0000

    DEVINETTE
    DEVINETTE

---
+++ devinette.js
@@ -0,0 +1,35 @@
+/* 
+Activité : jeu de devinette
+*/
+ console.log("Bienvenue dans ce jeu de devinette !");
+ // Cette ligne génère aléatoirement un nombre entre 1 et 100
+var solution = Math.floor(Math.random() * 100) + 1;
+ // Décommentez temporairement cette ligne pour mieux vérifier le programme
+//console.log("(La solution est " + solution + ")");
+ // TODO : complétez le programme
+// initialisation
+var proposition=-solution;
+var essai=0;
+ var ESSAI_MAX=6; //nombre maximum d'essai
+ do {             
+            proposition = Number(prompt("Entrez un nombre entre 1 et 100:"));
+             if (proposition<solution)
+            {
+                alert(proposition+" est trop petit.");
+            }
+            else if (proposition>solution)
+            {
+                alert(proposition+" est trop grand.");
+            }
+             else
+            {
+              alert("Bravo ! La solution était "+solution+".");
+              exit;
+            }
+        
+    essai++;
+        
+    
+} while(essai<ESSAI_MAX &&  proposition!=solution);
+  
+alert("Perdu ... La solution était "+solution+".");
